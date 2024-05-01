0609c6cf6a21a72fa3295e4d4260628071db0e71
Felipe Martinez <pipernene@gmail.com>
2024-05-01 14:03:29 +0000

    Add freertos firmware stuff

--- test/firmware/main.c
+++ test/firmware/main.c
@@ -1,6 +1,23 @@
+void SVC_Handler()
+{
+
+}
+
 int main()
 {
+    // vTaskStartScheduler();
+
+    __asm volatile("svc 0");
+
     for (;;)
     {
     }
+}
+
+void vApplicationMallocFailedHook()
+{
+}
+
+void vApplicationStackOverflowHook(void *xTask, char *pcTaskName)
+{
 }
