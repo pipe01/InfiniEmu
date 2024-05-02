931a3d6728a9ca14eb45b3da43242ed3d6b1c6c8
Felipe Martinez <pipernene@gmail.com>
2024-05-02 20:11:03 +0200

    Add register utils

--- include/byte_util.h
+++ include/byte_util.h
@@ -1,5 +1,31 @@
 #pragma once
 
+#include <stdint.h>
+
+#if !defined(__BYTE_ORDER__) || (__BYTE_ORDER__ != __ORDER_LITTLE_ENDIAN__)
+#error This program only works on little endian systems
+#endif
+
+typedef union {
+    struct {
+        uint8_t a : 8;
+        uint8_t b : 8;
+        uint8_t c : 8;
+        uint8_t d : 8;
+    };
+    uint8_t u8[4];
+    uint32_t u32;
+} Register4;
+
+typedef union {
+    struct {
+        uint16_t a : 16;
+        uint16_t b : 16;
+    };
+    uint16_t u16[2];
+    uint32_t u32;
+} Register2;
+
 #define READ_UINT16(arr, addr) (arr[(addr)] | (arr[(addr)+1] << 8))
 #define WRITE_UINT16(arr, addr, value) do { \
     (arr)[(addr)] = (value) & 0xFF; \
