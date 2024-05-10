c9375e45dacf896cddccd56a69acd31ce5e2baab
Felipe Martinez <pipernene@gmail.com>
2024-05-10 12:40:29 +0000

    Use segger rtt on test firmware

--- test/firmware/main.c
+++ test/firmware/main.c
@@ -1,17 +1,34 @@
-void SVC_Handler()
-{
+#include <FreeRTOS.h>
+#include <task.h>
 
+#include <SEGGER_RTT.h>
+
+void vTaskCode(void *)
+{
+  for (;;)
+  {
+    __asm volatile("nop");
+    SEGGER_RTT_WriteString(0, "Tick\n");
+    vTaskDelay(pdMS_TO_TICKS(1000));
+  }
 }
 
 int main()
 {
-    // vTaskStartScheduler();
+  SEGGER_RTT_ConfigUpBuffer(0, NULL, NULL, 0, SEGGER_RTT_MODE_BLOCK_IF_FIFO_FULL);
 
-    __asm volatile("svc 0");
+  SEGGER_RTT_WriteString(0, "SEGGER Real-Time-Terminal Sample\n");
 
-    for (;;)
-    {
-    }
+  TaskHandle_t xHandle = NULL;
+  xTaskCreate(vTaskCode, "NAME", 300, NULL, 1, &xHandle);
+
+  vTaskStartScheduler();
+
+  __asm volatile("svc 0");
+
+  for (;;)
+  {
+  }
 }
 
 void vApplicationMallocFailedHook()
