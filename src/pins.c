78ac35b0e1426d97531f6409b7f81dfaf208a4fc
Felipe Martinez <pipernene@gmail.com>
2024-06-01 12:34:43 +0000

    Remove debug prints

--- src/pins.c
+++ src/pins.c
@@ -4,7 +4,6 @@
 #include <stdint.h>
 #include <stdlib.h>
 #include <string.h>
-#include <stdio.h>
 
 typedef struct
 {
@@ -50,10 +49,7 @@ static inline void pins_set_state(pins_t *pins, int pin, bool is_set)
     p->is_set = is_set;
 
     if (p->sense != SENSE_DISABLED && ((is_set && (p->sense == SENSE_HIGH)) || (!is_set && (p->sense == SENSE_LOW))))
-    {
-        printf("Sense triggered on pin %d\n", pin);
         pins->latch |= 1 << pin;
-    }
 }
 
 void pins_set(pins_t *pins, int pin)
@@ -102,8 +98,6 @@ void pins_set_output(pins_t *pins, int pin)
 void pins_set_sense(pins_t *pins, int pin, pinsense_t sense)
 {
     assert(pin >= 0 && pin < PINS_COUNT);
-
-    printf("Setting sense for pin %d to %d\n", pin, sense);
 
     pins->pins[pin].sense = sense;
 }
