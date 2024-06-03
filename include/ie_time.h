9dddaaee5b7f5851382859fe2302303d2a732db7
Felipe Martinez <pipernene@gmail.com>
2024-06-03 23:05:14 +0000

    Include microseconds_now as inline func

--- include/ie_time.h
+++ include/ie_time.h
@@ -2,4 +2,26 @@
 
 #include <stdint.h>
 
-uint64_t microseconds_now();
+#ifdef __EMSCRIPTEN__
+
+#include <emscripten.h>
+
+static inline uint64_t microseconds_now()
+{
+    return EM_ASM_INT(return performance.now() * 1e3);
+}
+
+#else
+
+#include <stddef.h>
+#include <sys/time.h>
+
+static inline uint64_t microseconds_now()
+{
+    static _Thread_local struct timeval tv;
+
+    gettimeofday(&tv, NULL);
+    return tv.tv_sec * 1e6 + tv.tv_usec;
+}
+
+#endif
