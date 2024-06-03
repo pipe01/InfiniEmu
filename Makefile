f31dc91c4900d5e2f12cfebba2222016b4a20c0b
Felipe Martinez <pipernene@gmail.com>
2024-06-03 23:29:24 +0000

    Press buttons

--- Makefile
+++ Makefile
@@ -19,6 +19,7 @@ LIBS = -lm
 WASM_FUNCS = malloc pinetime_new pinetime_step pinetime_loop
 WASM_FUNCS += pinetime_get_st7789 st7789_read_screen st7789_is_sleeping st7789_get_write_count
 WASM_FUNCS += pinetime_get_cst816s cst816s_do_touch cst816s_release_touch
+WASM_FUNCS += pinetime_get_nrf52832 nrf52832_get_pins pins_set pins_clear
 
 ifeq ($(WASM), 1)
 	CFLAGS += -I/usr/include/capstone
@@ -67,7 +68,7 @@ infiniemu: $(OBJ) obj/src/infiniemu.o
 	$(CC) -o $@ $^ -static $(CFLAGS) $(LIBS)
 
 infiniemu.wasm: $(OBJ)
-	$(CC) -o infiniemu.js -O3 -sTOTAL_STACK=64MB -sALLOW_MEMORY_GROWTH -sEXPORTED_RUNTIME_METHODS=ccall,cwrap -sEXPORTED_FUNCTIONS=$(_WASM_FUNCS) capstone-5.0.1/libcapstone.a $^
+	$(CC) -o infiniemu.js -sTOTAL_STACK=64MB -sALLOW_MEMORY_GROWTH -sEXPORTED_RUNTIME_METHODS=ccall,cwrap -sEXPORTED_FUNCTIONS=$(_WASM_FUNCS) capstone-5.0.1/libcapstone.a $^
 
 libinfiniemu.o: $(OBJ)
 	ld -relocatable -static $^ -o $@
--- infiniemu.worker.js
+++ infiniemu.worker.js
@@ -11,9 +11,9 @@ var Module = {
 
 importScripts("infiniemu.js");
 
-const iterations = 1000000;
+const iterations = 700000;
 
-var pt, lcd, touch;
+var pt, lcd, touch, pins;
 
 var displayBufferPointer;
 
@@ -34,10 +34,23 @@ onmessage = (e) => {
     switch (e.data.type) {
         case "doTouch":
             Module._cst816s_do_touch(touch, e.data.gesture, e.data.x, e.data.y);
-            setTimeout(() => Module._cst816s_release_touch(touch), e.data.duration);
 
+            if (e.data.duration)
+                setTimeout(() => Module._cst816s_release_touch(touch), e.data.duration);
+            break;
+
+        case "clearTouch":
+            Module._cst816s_release_touch(touch);
             break;
         
+        case "pressButton":
+            Module._pins_set(pins, 13);
+            break;
+
+        case "releaseButton":
+            Module._pins_clear(pins, 13);
+            break;
+
         case "loadProgramFile":
             if (pt) {
                 console.error("Pinetime already loaded");
@@ -51,6 +64,7 @@ onmessage = (e) => {
                 pt = Module.ccall("pinetime_new", "any", ["array", "number", "boolean"], [program, program.length, true]);
                 lcd = Module._pinetime_get_st7789(pt);
                 touch = Module._pinetime_get_cst816s(pt);
+                pins = Module._nrf52832_get_pins(Module._pinetime_get_nrf52832(pt));
 
                 displayBufferPointer = Module._malloc(240 * 240 * 2);
 
