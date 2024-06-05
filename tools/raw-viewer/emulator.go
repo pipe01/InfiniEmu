2da658bb798bb9d3c8a170def8e145f74e173af4
Felipe Martinez <pipernene@gmail.com>
2024-06-05 22:10:33 +0200

    Refactor viewer

---
+++ tools/raw-viewer/emulator.go
@@ -0,0 +1,356 @@
+package main
+
+/*
+#cgo CFLAGS: -I../../include
+#cgo LDFLAGS: libinfiniemu.o -lcapstone
+
+#include "gdb.h"
+#include "pinetime.h"
+#include "scheduler.h"
+
+volatile unsigned long inst_counter = 0;
+volatile bool stop_loop = false;
+
+static void loop(pinetime_t *pt)
+{
+	stop_loop = false;
+
+	while (!stop_loop)
+	{
+		pinetime_step(pt);
+		inst_counter++;
+	}
+}
+
+static scheduler_t *create_sched(pinetime_t *pt, size_t freq)
+{
+	return scheduler_new((scheduler_cb_t)pinetime_step, pt, freq);
+}
+*/
+import "C"
+
+import (
+	"runtime"
+	"sync/atomic"
+	"time"
+)
+
+const (
+	displayWidth         = 240
+	displayHeight        = 240
+	displayBytesPerPixel = C.BYTES_PER_PIXEL
+)
+
+type TouchGesture int
+
+const (
+	GestureNone       TouchGesture = C.GESTURE_NONE
+	GestureSlideDown  TouchGesture = C.GESTURE_SLIDEDOWN
+	GestureSlideUp    TouchGesture = C.GESTURE_SLIDEUP
+	GestureSlideLeft  TouchGesture = C.GESTURE_SLIDELEFT
+	GestureSlideRight TouchGesture = C.GESTURE_SLIDERIGHT
+	GestureSingleTap  TouchGesture = C.GESTURE_SINGLETAP
+	GestureDoubleTap  TouchGesture = C.GESTURE_DOUBLETAP
+	GestureLongPress  TouchGesture = C.GESTURE_LONGPRESS
+)
+
+type RunMode int
+
+const (
+	RunModeLoop RunMode = iota
+	RunModeScheduled
+	RunModeGDB
+)
+
+type Brightness int
+
+const (
+	BrightnessOff Brightness = iota
+	BrightnessLow
+	BrightnessMedium
+	BrightnessHigh
+	BrightnessInvalid
+)
+
+func (b Brightness) String() string {
+	switch b {
+	case BrightnessOff:
+		return "off"
+	case BrightnessLow:
+		return "low"
+	case BrightnessMedium:
+		return "medium"
+	case BrightnessHigh:
+		return "high"
+	default:
+		return "unknown"
+	}
+}
+
+type RTCTracker struct {
+	rtc *C.RTC_t
+
+	TicksPerSecond       uint32
+	TargetTicksPerSecond uint32
+	Running              bool
+
+	lastTicks uint32
+}
+
+func (r *RTCTracker) update(updateInterval time.Duration) {
+	r.Running = C.rtc_is_running(r.rtc) != 0
+	if !r.Running {
+		return
+	}
+
+	ticks := uint32(C.rtc_get_counter(r.rtc))
+
+	interval := time.Duration(C.rtc_get_tick_interval_us(r.rtc)) * time.Microsecond
+	if interval == 0 {
+		return
+	}
+
+	r.TargetTicksPerSecond = 1e6 / uint32(interval.Microseconds())
+	r.TicksPerSecond = (1e6 * (ticks - r.lastTicks)) / uint32(updateInterval.Microseconds())
+
+	r.lastTicks = ticks
+}
+
+type CPUVariable struct {
+	mem *C.memreg_t
+	sym *Symbol
+}
+
+func (v *CPUVariable) Read() uint64 {
+	if v.sym == nil {
+		return 0
+	}
+
+	switch v.sym.Length {
+	case 1:
+		return uint64(C.memreg_read_byte(v.mem, C.uint(v.sym.Start)))
+	case 2:
+		return uint64(C.memreg_read_halfword(v.mem, C.uint(v.sym.Start)))
+	case 4:
+		return uint64(C.memreg_read(v.mem, C.uint(v.sym.Start)))
+	case 8:
+		return uint64(C.memreg_read(v.mem, C.uint(v.sym.Start))) | (uint64(C.memreg_read(v.mem, C.uint(v.sym.Start+4))) << 32)
+	default:
+		panic("unsupported length")
+	}
+}
+
+func (v *CPUVariable) Write(value uint64) {
+	if v.sym == nil {
+		return
+	}
+
+	switch v.sym.Length {
+	case 1, 2, 4:
+		C.memreg_write(v.mem, C.uint(v.sym.Start), C.uint(value), C.byte_size_t(v.sym.Length))
+	case 8:
+		C.memreg_write(v.mem, C.uint(v.sym.Start), C.uint(value), C.SIZE_WORD)
+		C.memreg_write(v.mem, C.uint(v.sym.Start+4), C.uint(value>>32), C.SIZE_WORD)
+	}
+}
+
+type Emulator struct {
+	program *Program
+
+	pt          *C.pinetime_t
+	nrf52       *C.NRF52832_t
+	cpu         *C.cpu_t
+	lcd         *C.st7789_t
+	touchScreen *C.cst816s_t
+	pins        *C.pins_t
+	rtcs        []*C.RTC_t
+
+	sched          *C.scheduler_t
+	isRunning      atomic.Bool
+	currentRunMode RunMode
+
+	instPerSecond uint64
+}
+
+func NewEmulator(program *Program) *Emulator {
+	flash := program.Flatten()
+
+	var pinner runtime.Pinner
+	defer pinner.Unpin()
+
+	pinner.Pin(&flash[0])
+
+	// The C code makes a copy of the flash contents, so we can unpin it after
+	pt := C.pinetime_new((*C.uchar)(&flash[0]), C.ulong(len(flash)), true)
+	C.pinetime_reset(pt)
+
+	nrf52 := C.pinetime_get_nrf52832(pt)
+	pins := C.nrf52832_get_pins(nrf52)
+
+	// Active low pins with pull ups
+	C.pins_set(pins, pinCharging)
+	C.pins_set(pins, pinPowerPresent)
+
+	return &Emulator{
+		program: program,
+		pt:      pt,
+		sched:   C.create_sched(pt, baseFrequencyHZ),
+
+		cpu:         C.nrf52832_get_cpu(nrf52),
+		nrf52:       nrf52,
+		lcd:         C.pinetime_get_st7789(pt),
+		touchScreen: C.pinetime_get_cst816s(pt),
+		pins:        pins,
+		rtcs: []*C.RTC_t{
+			(*C.RTC_t)(C.nrf52832_get_peripheral(nrf52, C.INSTANCE_RTC0)),
+			(*C.RTC_t)(C.nrf52832_get_peripheral(nrf52, C.INSTANCE_RTC1)),
+			(*C.RTC_t)(C.nrf52832_get_peripheral(nrf52, C.INSTANCE_RTC2)),
+		},
+	}
+}
+
+func (e *Emulator) perfLoop() {
+	interval := 500 * time.Millisecond
+
+	var lastCounter uint64
+
+	rtcs := make([]*RTCTracker, len(e.rtcs))
+	for i := range e.rtcs {
+		rtcs[i] = e.CreateRTCTracker(i)
+	}
+
+	for range time.Tick(interval) {
+		if !e.isRunning.Load() {
+			break
+		}
+
+		for _, rtc := range rtcs {
+			rtc.update(interval)
+		}
+
+		var instCounter uint64
+		if e.currentRunMode == RunModeLoop {
+			instCounter = uint64(C.inst_counter)
+		} else {
+			instCounter = uint64(C.scheduler_get_counter(e.sched))
+		}
+
+		e.instPerSecond = (1e6 * (instCounter - lastCounter)) / uint64(interval.Microseconds())
+		lastCounter = instCounter
+	}
+}
+
+func (e *Emulator) CreateRTCTracker(rtcNum int) *RTCTracker {
+	return &RTCTracker{
+		rtc: e.rtcs[rtcNum],
+	}
+}
+
+func (e *Emulator) Brightness() Brightness {
+	lcdLow := bool(C.pins_is_set(e.pins, pinLcdBacklightLow))
+	lcdMedium := bool(C.pins_is_set(e.pins, pinLcdBacklightMedium))
+	lcdHigh := bool(C.pins_is_set(e.pins, pinLcdBacklightHigh))
+
+	if !lcdLow && lcdMedium && lcdHigh {
+		return BrightnessLow
+	} else if !lcdLow && !lcdMedium && lcdHigh {
+		return BrightnessMedium
+	} else if !lcdLow && !lcdMedium && !lcdHigh {
+		return BrightnessHigh
+	} else if lcdLow && lcdMedium && lcdHigh {
+		return BrightnessOff
+	}
+
+	return BrightnessInvalid
+}
+
+func (e *Emulator) Variable(name string) *CPUVariable {
+	return &CPUVariable{
+		mem: C.cpu_mem(e.cpu),
+		sym: e.program.FindSymbol(name),
+	}
+}
+
+func (e *Emulator) Start(mode RunMode) {
+	if !e.isRunning.CompareAndSwap(false, true) {
+		panic("emulator already running")
+	}
+
+	e.currentRunMode = mode
+
+	switch mode {
+	case RunModeLoop:
+		go C.loop(e.pt)
+
+	case RunModeScheduled:
+		go C.scheduler_run(e.sched)
+
+	case RunModeGDB:
+		gdb := C.gdb_new(e.pt, true)
+		go C.gdb_start(gdb)
+	}
+}
+
+func (e *Emulator) Stop() {
+	if !e.isRunning.CompareAndSwap(true, false) {
+		return
+	}
+
+	switch e.currentRunMode {
+	case RunModeLoop:
+		C.stop_loop = true
+
+	case RunModeScheduled:
+		C.scheduler_stop(e.sched)
+
+	case RunModeGDB:
+		panic("can't stop gdb")
+	}
+
+	time.Sleep(100 * time.Millisecond) // TODO: Properly wait for loop or scheduler to stop
+}
+
+func (e *Emulator) InstructionsPerSecond() uint64 {
+	return e.instPerSecond
+}
+
+func (e *Emulator) SetFrequency(hz uint) {
+	C.scheduler_set_frequency(e.sched, C.size_t(hz))
+}
+
+func (e *Emulator) DoTouch(gesture TouchGesture, x, y int) {
+	C.cst816s_do_touch(e.touchScreen, C.touch_gesture_t(gesture), C.uint16_t(x), C.uint16_t(y))
+}
+
+func (e *Emulator) ReleaseTouch() {
+	C.cst816s_release_touch(e.touchScreen)
+}
+
+func (e *Emulator) PinSet(pin int) {
+	C.pins_set(e.pins, C.int(pin))
+}
+
+func (e *Emulator) PinClear(pin int) {
+	C.pins_clear(e.pins, C.int(pin))
+}
+
+func (e *Emulator) IsPinSet(pin int) bool {
+	return bool(C.pins_is_set(e.pins, C.int(pin)))
+}
+
+func (e *Emulator) IsDisplaySleeping() bool {
+	return bool(C.st7789_is_sleeping(e.lcd))
+}
+
+func (e *Emulator) ReadDisplayBuffer(p []byte) {
+	if len(p) != displayWidth*displayHeight*displayBytesPerPixel {
+		panic("invalid buffer size")
+	}
+
+	var pinner runtime.Pinner
+	defer pinner.Unpin()
+
+	pinner.Pin(&p[0])
+
+	C.st7789_read_screen(e.lcd, (*C.uchar)(&p[0]), displayWidth, displayHeight)
+}
--- tools/raw-viewer/main.go
+++ tools/raw-viewer/main.go
@@ -9,47 +9,11 @@ import (
 	"log"
 	"math"
 	"os"
-	"runtime"
 	"strconv"
 	"strings"
 	"time"
 
 	"github.com/AllenDang/imgui-go"
-)
-
-/*
-#cgo CFLAGS: -I../../include
-#cgo LDFLAGS: libinfiniemu.o -lcapstone
-
-#include "gdb.h"
-#include "pinetime.h"
-#include "scheduler.h"
-
-volatile unsigned long inst_counter = 0;
-volatile bool stop_loop = false;
-
-void loop(pinetime_t *pt)
-{
-	stop_loop = false;
-
-	while (!stop_loop)
-	{
-		pinetime_step(pt);
-		inst_counter++;
-	}
-}
-
-scheduler_t *create_sched(pinetime_t *pt, size_t freq)
-{
-	return scheduler_new((scheduler_cb_t)pinetime_step, pt, freq);
-}
-*/
-import "C"
-
-const (
-	displayWidth         = 240
-	displayHeight        = 240
-	displayBytesPerPixel = C.BYTES_PER_PIXEL
 )
 
 const (
@@ -74,30 +38,6 @@ const (
 	pinTwiScl             = 7
 	pinTwiSda             = 6
 )
-
-type Brightness int
-
-const (
-	BrightnessOff Brightness = iota
-	BrightnessLow
-	BrightnessMedium
-	BrightnessHigh
-)
-
-func (b Brightness) String() string {
-	switch b {
-	case BrightnessOff:
-		return "off"
-	case BrightnessLow:
-		return "low"
-	case BrightnessMedium:
-		return "medium"
-	case BrightnessHigh:
-		return "high"
-	default:
-		return "unknown"
-	}
-}
 
 const baseFrequencyHZ = 18_000_000
 
@@ -124,98 +64,18 @@ func convertImage(raw []byte) *image.RGBA {
 	return img
 }
 
-type RTCTracker struct {
-	rtc *C.RTC_t
-
-	TicksPerSecond       uint32
-	TargetTicksPerSecond uint32
-	Running              bool
-
-	lastTicks uint32
-} //
-
-func NewRTCTracker(rtc *C.RTC_t) *RTCTracker {
-	return &RTCTracker{
-		rtc: rtc,
-	}
-}
-
-func (r *RTCTracker) Update(updateInterval time.Duration) {
-	r.Running = C.rtc_is_running(r.rtc) != 0
-	if !r.Running {
-		return
-	}
-
-	ticks := uint32(C.rtc_get_counter(r.rtc))
-
-	interval := time.Duration(C.rtc_get_tick_interval_us(r.rtc)) * time.Microsecond
-	if interval == 0 {
-		return
-	}
-
-	r.TargetTicksPerSecond = 1e6 / uint32(interval.Microseconds())
-	r.TicksPerSecond = (1e6 * (ticks - r.lastTicks)) / uint32(updateInterval.Microseconds())
-
-	r.lastTicks = ticks
-}
-
-type CPUVariable struct {
-	mem *C.memreg_t
-	sym *Symbol
-}
-
-func NewCPUVariable(pt *C.pinetime_t, program *Program, name string) *CPUVariable {
-	return &CPUVariable{
-		mem: C.cpu_mem(C.nrf52832_get_cpu(C.pinetime_get_nrf52832(pt))),
-		sym: program.FindSymbol(name),
-	}
-}
-
-func (v *CPUVariable) Read() uint64 {
-	if v.sym == nil {
-		return 0
-	}
-
-	switch v.sym.Length {
-	case 1:
-		return uint64(C.memreg_read_byte(v.mem, C.uint(v.sym.Start)))
-	case 2:
-		return uint64(C.memreg_read_halfword(v.mem, C.uint(v.sym.Start)))
-	case 4:
-		return uint64(C.memreg_read(v.mem, C.uint(v.sym.Start)))
-	case 8:
-		return uint64(C.memreg_read(v.mem, C.uint(v.sym.Start))) | (uint64(C.memreg_read(v.mem, C.uint(v.sym.Start+4))) << 32)
-	default:
-		panic("unsupported length")
-	}
-}
-
-func (v *CPUVariable) Write(value uint64) {
-	if v.sym == nil {
-		return
-	}
-
-	switch v.sym.Length {
-	case 1, 2, 4:
-		C.memreg_write(v.mem, C.uint(v.sym.Start), C.uint(value), C.byte_size_t(v.sym.Length))
-	case 8:
-		C.memreg_write(v.mem, C.uint(v.sym.Start), C.uint(value), C.SIZE_WORD)
-		C.memreg_write(v.mem, C.uint(v.sym.Start+4), C.uint(value>>32), C.SIZE_WORD)
-	}
-}
-
 func constCheckbox(id string, state bool) {
 	imgui.Checkbox(id, &state)
 }
 
-func pinCheckbox(id string, pins *C.pins_t, pin int32) {
-	state := bool(C.pins_is_set(pins, C.int(pin)))
+func pinCheckbox(id string, emulator *Emulator, pin int) {
+	state := emulator.IsPinSet(pin)
 
 	if imgui.Checkbox(id, &state) {
 		if state {
-			C.pins_set(pins, C.int(pin))
+			emulator.PinSet(pin)
 		} else {
-			C.pins_clear(pins, C.int(pin))
+			emulator.PinClear(pin)
 		}
 	}
 }
@@ -251,10 +111,6 @@ func loadFlash(filePath string) (*Program, error) {
 var platform *imgui.GLFW
 var renderer *imgui.OpenGL3
 
-var lcd *C.st7789_t
-var touchScreen *C.cst816s_t
-var pins *C.pins_t
-
 var allowScreenSwipes = true
 var screenTextureID imgui.TextureID
 var screenMouseDownPos imgui.Vec2
@@ -263,7 +119,7 @@ var screenDidSwipe bool
 var mouseLeftIsDown, mouseLeftWasDown bool
 var mouseRightIsDown, mouseRightWasDown bool
 
-func screenWindow(screenBuffer []byte, brightness Brightness) {
+func screenWindow(screenBuffer []byte, emulator *Emulator) {
 	var err error
 
 	flags := imgui.WindowFlagsNoResize | imgui.WindowFlagsAlwaysAutoResize
@@ -276,10 +132,11 @@ func screenWindow(screenBuffer []byte, brightness Brightness) {
 		renderer.ReleaseImage(screenTextureID)
 		var img *image.RGBA
 
-		if C.st7789_is_sleeping(lcd) || brightness == BrightnessOff {
+		if emulator.IsDisplaySleeping() || emulator.Brightness() == BrightnessOff {
 			img = blackScreenImage
 		} else {
-			C.st7789_read_screen(lcd, (*C.uchar)(&screenBuffer[0]), displayWidth, displayHeight)
+			emulator.ReadDisplayBuffer(screenBuffer)
+
 			img = convertImage(screenBuffer)
 		}
 
@@ -296,7 +153,7 @@ func screenWindow(screenBuffer []byte, brightness Brightness) {
 				screenDidSwipe = false
 
 				if !allowScreenSwipes {
-					C.cst816s_do_touch(touchScreen, C.GESTURE_SINGLETAP, C.ushort(screenMouseDownPos.X), C.ushort(screenMouseDownPos.Y))
+					emulator.DoTouch(GestureSingleTap, int(screenMouseDownPos.X), int(screenMouseDownPos.Y))
 				}
 			} else if mouseLeftIsDown && mouseLeftWasDown && !screenDidSwipe && allowScreenSwipes {
 				pos := imgui.MousePos().Minus(imgui.GetItemRectMin())
@@ -306,40 +163,40 @@ func screenWindow(screenBuffer []byte, brightness Brightness) {
 				if dist > 20 {
 					screenDidSwipe = true
 
-					var gesture C.touch_gesture_t
+					var gesture TouchGesture
 
 					xDist := math.Abs(float64(distVec.X))
 					yDist := math.Abs(float64(distVec.Y))
 
 					if xDist > yDist {
 						if distVec.X > 0 {
-							gesture = C.GESTURE_SLIDERIGHT
+							gesture = GestureSlideRight
 						} else {
-							gesture = C.GESTURE_SLIDELEFT
+							gesture = GestureSlideLeft
 						}
 					} else {
 						if distVec.Y > 0 {
-							gesture = C.GESTURE_SLIDEDOWN
+							gesture = GestureSlideDown
 						} else {
-							gesture = C.GESTURE_SLIDEUP
+							gesture = GestureSlideUp
 						}
 					}
 
-					C.cst816s_do_touch(touchScreen, gesture, C.ushort(pos.X), C.ushort(pos.Y))
+					emulator.DoTouch(gesture, int(pos.X), int(pos.Y))
 				}
 			} else if !mouseLeftIsDown && mouseLeftWasDown {
 				if allowScreenSwipes && !screenDidSwipe {
-					C.cst816s_do_touch(touchScreen, C.GESTURE_SINGLETAP, C.ushort(screenMouseDownPos.X), C.ushort(screenMouseDownPos.Y))
+					emulator.DoTouch(GestureSingleTap, int(screenMouseDownPos.X), int(screenMouseDownPos.Y))
 					time.Sleep(50 * time.Millisecond) // TODO: Do this better?
 				}
 
-				C.cst816s_release_touch(touchScreen)
+				emulator.ReleaseTouch()
 			}
 
 			if mouseRightIsDown && !mouseRightWasDown {
-				C.pins_set(pins, pinButton)
+				emulator.PinSet(pinButton)
 			} else if !mouseRightIsDown && mouseRightWasDown {
-				C.pins_clear(pins, pinButton)
+				emulator.PinClear(pinButton)
 			}
 		}
 
@@ -347,7 +204,7 @@ func screenWindow(screenBuffer []byte, brightness Brightness) {
 
 		imgui.Separator()
 
-		imgui.Text(fmt.Sprintf("Brightness: %s", brightness.String()))
+		imgui.Text(fmt.Sprintf("Brightness: %v", emulator.Brightness()))
 	}
 	imgui.End()
 }
@@ -370,67 +227,18 @@ func main() {
 		log.Fatal(err)
 	}
 
-	flash := program.Flatten()
+	emulator := NewEmulator(program)
 
-	var pinner runtime.Pinner
-	pinner.Pin(&program)
-
-	pt := C.pinetime_new((*C.uchar)(&flash[0]), C.ulong(len(flash)), true)
-	C.pinetime_reset(pt)
-
-	pinner.Unpin()
-
-	sched := C.create_sched(pt, baseFrequencyHZ)
-
-	if *runGDB {
-		gdb := C.gdb_new(pt, true)
-		go C.gdb_start(gdb)
-	} else if noScheduler {
-		go C.loop(pt)
-	} else {
-		go C.scheduler_run(sched)
-	}
-
-	lcd = C.pinetime_get_st7789(pt)
-	touchScreen = C.pinetime_get_cst816s(pt)
-	pins = C.nrf52832_get_pins(C.pinetime_get_nrf52832(pt))
 	rtcs := []*RTCTracker{
-		NewRTCTracker((*C.RTC_t)(C.nrf52832_get_peripheral(C.pinetime_get_nrf52832(pt), C.INSTANCE_RTC0))),
-		NewRTCTracker((*C.RTC_t)(C.nrf52832_get_peripheral(C.pinetime_get_nrf52832(pt), C.INSTANCE_RTC1))),
-		NewRTCTracker((*C.RTC_t)(C.nrf52832_get_peripheral(C.pinetime_get_nrf52832(pt), C.INSTANCE_RTC2))),
+		emulator.CreateRTCTracker(0),
+		emulator.CreateRTCTracker(1),
+		emulator.CreateRTCTracker(2),
 	}
 
-	freertosFreeBytesRemaining := NewCPUVariable(pt, program, "xFreeBytesRemaining")
+	freertosFreeBytesRemaining := emulator.Variable("xFreeBytesRemaining")
 
-	NewCPUVariable(pt, program, "NoInit_MagicWord").Write(0xDEAD0000)
-	NewCPUVariable(pt, program, "NoInit_BackUpTime").Write(uint64(time.Now().UnixNano()))
-
-	// Active low pins with pull ups
-	C.pins_set(pins, pinCharging)
-	C.pins_set(pins, pinPowerPresent)
-
-	var instPerSecond uint64
-	go func() {
-		interval := 500 * time.Millisecond
-
-		var lastCounter uint64
-
-		for range time.Tick(interval) {
-			for _, rtc := range rtcs {
-				rtc.Update(interval)
-			}
-
-			var instCounter uint64
-			if noScheduler {
-				instCounter = uint64(C.inst_counter)
-			} else {
-				instCounter = uint64(C.scheduler_get_counter(sched))
-			}
-
-			instPerSecond = (1e6 * (instCounter - lastCounter)) / uint64(interval.Microseconds())
-			lastCounter = instCounter
-		}
-	}()
+	emulator.Variable("NoInit_MagicWord").Write(0xDEAD0000)
+	emulator.Variable("NoInit_BackUpTime").Write(uint64(time.Now().UnixNano()))
 
 	screenBuffer := make([]byte, displayWidth*displayHeight*displayBytesPerPixel)
 
@@ -465,11 +273,17 @@ func main() {
 
 	var releaseTouchTime time.Time
 
-	brightness := BrightnessOff
-
 	var speed float32 = 1
 
 	freeHeapHistory := make([]float64, 0)
+
+	if *runGDB {
+		emulator.Start(RunModeGDB)
+	} else if noScheduler {
+		emulator.Start(RunModeLoop)
+	} else {
+		emulator.Start(RunModeScheduled)
+	}
 
 	for !platform.ShouldStop() {
 		<-t
@@ -479,7 +293,8 @@ func main() {
 
 		if !releaseTouchTime.IsZero() && time.Now().After(releaseTouchTime) {
 			releaseTouchTime = time.Time{}
-			C.cst816s_release_touch(touchScreen)
+
+			emulator.ReleaseTouch()
 		}
 
 		platform.ProcessEvents()
@@ -487,30 +302,16 @@ func main() {
 		platform.NewFrame()
 		imgui.NewFrame()
 
-		lcdLow := bool(C.pins_is_set(pins, pinLcdBacklightLow))
-		lcdMedium := bool(C.pins_is_set(pins, pinLcdBacklightMedium))
-		lcdHigh := bool(C.pins_is_set(pins, pinLcdBacklightHigh))
-
-		if !lcdLow && lcdMedium && lcdHigh {
-			brightness = BrightnessLow
-		} else if !lcdLow && !lcdMedium && lcdHigh {
-			brightness = BrightnessMedium
-		} else if !lcdLow && !lcdMedium && !lcdHigh {
-			brightness = BrightnessHigh
-		} else {
-			brightness = BrightnessOff
-		}
-
-		screenWindow(screenBuffer, brightness)
+		screenWindow(screenBuffer, emulator)
 
 		imgui.SetNextWindowPosV(imgui.Vec2{X: 300, Y: 20}, imgui.ConditionOnce, imgui.Vec2{})
 		if imgui.BeginV("Inputs", nil, imgui.WindowFlagsAlwaysAutoResize) {
 			imgui.Button("Side button")
 			if imgui.IsItemHovered() {
 				if mouseLeftIsDown && !mouseLeftWasDown {
-					C.pins_set(pins, pinButton)
+					emulator.PinSet(pinButton)
 				} else if !mouseLeftIsDown && mouseLeftWasDown {
-					C.pins_clear(pins, pinButton)
+					emulator.PinClear(pinButton)
 				}
 			}
 
@@ -523,26 +324,26 @@ func main() {
 					imgui.TableNextRow(0, 0)
 					imgui.TableSetColumnIndex(1)
 					if imgui.Button("Slide up") {
-						C.cst816s_do_touch(touchScreen, C.GESTURE_SLIDEUP, displayWidth/2, displayHeight/2)
+						emulator.DoTouch(GestureSlideUp, displayWidth/2, displayHeight/2)
 						releaseTouchTime = time.Now().Add(touchDuration)
 					}
 
 					imgui.TableNextRow(0, 0)
 					imgui.TableSetColumnIndex(0)
 					if imgui.Button("Slide left") {
-						C.cst816s_do_touch(touchScreen, C.GESTURE_SLIDELEFT, displayWidth/2, displayHeight/2)
+						emulator.DoTouch(GestureSlideLeft, displayWidth/2, displayHeight/2)
 						releaseTouchTime = time.Now().Add(touchDuration)
 					}
 					imgui.TableSetColumnIndex(2)
 					if imgui.Button("Slide right") {
-						C.cst816s_do_touch(touchScreen, C.GESTURE_SLIDERIGHT, displayWidth/2, displayHeight/2)
+						emulator.DoTouch(GestureSlideRight, displayWidth/2, displayHeight/2)
 						releaseTouchTime = time.Now().Add(touchDuration)
 					}
 
 					imgui.TableNextRow(0, 0)
 					imgui.TableSetColumnIndex(1)
 					if imgui.Button("Slide down") {
-						C.cst816s_do_touch(touchScreen, C.GESTURE_SLIDEDOWN, displayWidth/2, displayHeight/2)
+						emulator.DoTouch(GestureSlideDown, displayWidth/2, displayHeight/2)
 						releaseTouchTime = time.Now().Add(touchDuration)
 					}
 				}
@@ -552,8 +353,8 @@ func main() {
 
 			imgui.Separator()
 
-			pinCheckbox("Charging (active low)", pins, pinCharging)
-			pinCheckbox("Power present (active low)", pins, pinPowerPresent)
+			pinCheckbox("Charging (active low)", emulator, pinCharging)
+			pinCheckbox("Power present (active low)", emulator, pinPowerPresent)
 		}
 		imgui.End()
 
@@ -575,32 +376,24 @@ func main() {
 				imgui.Separator()
 			}
 
-			imgui.LabelText(fmt.Sprint(instPerSecond), "Instructions per second")
+			imgui.LabelText(strconv.FormatUint(emulator.InstructionsPerSecond(), 10), "Instructions per second")
 
 			imgui.BeginDisabled(*runGDB)
 			{
 				if imgui.Checkbox("Disable scheduler", &noScheduler) {
+					emulator.Stop()
+
 					if noScheduler {
-						C.scheduler_stop(sched)
-
-						//TODO: Wait for scheduler to stop
-						time.Sleep(100 * time.Millisecond)
-
-						go C.loop(pt)
+						emulator.Start(RunModeLoop)
 					} else {
-						C.stop_loop = true
-
-						//TODO: Wait for loop to stop
-						time.Sleep(100 * time.Millisecond)
-
-						go C.scheduler_run(sched)
+						emulator.Start(RunModeScheduled)
 					}
 				}
 
 				imgui.BeginDisabled(noScheduler)
 				{
 					if imgui.SliderFloat("Speed", &speed, 0, 2) {
-						C.scheduler_set_frequency(sched, C.ulong(speed*baseFrequencyHZ))
+						emulator.SetFrequency(uint(speed * baseFrequencyHZ))
 					}
 				}
 				imgui.EndDisabled()
