7a408d310bff6e91da3f1f36b3f5b24d040bc126
Felipe Martinez <pipernene@gmail.com>
2024-06-26 02:13:47 +0200

    Make front page nicer

--- frontend/wasm/src/App.vue
+++ frontend/wasm/src/App.vue
@@ -1,14 +1,34 @@
 <template lang="pug">
 .container.mt-3
+    h1 InfiniEmu
+
     template(v-if="isLoading")
         h1 Loading...
     template(v-else-if="!pickedFile")
-        h2 Select a program file
+        h4 Select a program file
         .mb-3
             input.form-control(type="file" @change="loadFile($event.target.files[0])")
-        button.btn.btn-primary(@click="loadSampleFile") Load sample file
+
+        h4 or 
+            button.btn.btn-primary(@click="loadSampleFile") Load sample file
     template(v-else)
         Emulator(:programFile="pickedFile")
+
+    hr
+
+    p
+        | InfiniEmu is an emulator that emulates a full PineTime smartwatch, which includes:
+        ul
+            li NRF52832 along with its ARM Cortex M4 CPU and peripherals, 
+            li BMA425 I2C accelerometer
+            li CST816S I2C touch screen controller
+            li HRS3300 I2C heart rate sensor
+            li ST7789 SPI LCD display controller
+            li A generic SPI flash based on the XT25F32B-S
+
+    p This website hosts a WebAssembly version of the emulator, which is compiled from the original C code.
+
+    a(href="https://github.com/pipe01/InfiniEmu") View on GitHub
 </template>
 
 <script lang="ts" setup>
