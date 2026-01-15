6033891d08a61cb376def786b52bdbd3d4e08d75
Felipe Martinez <felipe@pipe01.net>
2026-01-16 00:29:31 +0100

    Formatting

--- include/memory.h
+++ include/memory.h
@@ -9,7 +9,8 @@
 
 #define MEMREG_FIND_NOT_FOUND 0xFFFFFFFF
 
-typedef enum __attribute__((packed)) {
+typedef enum __attribute__((packed))
+{
     SIZE_BYTE = 1,
     SIZE_HALFWORD = 2,
     SIZE_WORD = 4,
@@ -20,18 +21,20 @@ static inline uint32_t size_mask(byte_size_t size)
     return size == SIZE_WORD ? 0xFFFFFFFF : (uint32_t)((1 << (size * 8)) - 1);
 }
 
-typedef enum {
-    OP_RESET            = 0,
-    OP_LOAD_DATA        = 0xFF,
-    OP_READ_BYTE        = SIZE_BYTE,
-    OP_READ_HALFWORD    = SIZE_HALFWORD,
-    OP_READ_WORD        = SIZE_WORD,
-    OP_WRITE_BYTE       = -SIZE_BYTE,
-    OP_WRITE_HALFWORD   = -SIZE_HALFWORD,
-    OP_WRITE_WORD       = -SIZE_WORD,
+typedef enum
+{
+    OP_RESET = 0,
+    OP_LOAD_DATA = 0xFF,
+    OP_READ_BYTE = SIZE_BYTE,
+    OP_READ_HALFWORD = SIZE_HALFWORD,
+    OP_READ_WORD = SIZE_WORD,
+    OP_WRITE_BYTE = -SIZE_BYTE,
+    OP_WRITE_HALFWORD = -SIZE_HALFWORD,
+    OP_WRITE_WORD = -SIZE_WORD,
 } memreg_op_t;
 
-typedef enum {
+typedef enum
+{
     MEMREG_RESULT_OK,
     MEMREG_RESULT_OK_CONTINUE,
     MEMREG_RESULT_INVALID_ACCESS,
@@ -39,9 +42,9 @@ typedef enum {
     MEMREG_RESULT_UNHANDLED,
 } memreg_op_result_t;
 
-#define OP_IS_READ(op)      ((op) > 0)
-#define OP_IS_WRITE(op)     ((op) < 0)
-#define OP_IS_SIZE(op, size)    ((op) == OP_READ_##size || (op) == OP_WRITE_##size)
+#define OP_IS_READ(op) ((op) > 0)
+#define OP_IS_WRITE(op) ((op) < 0)
+#define OP_IS_SIZE(op, size) ((op) == OP_READ_##size || (op) == OP_WRITE_##size)
 
 #if ABORT_ON_INVALID_MEM_ACCESS
 #define OP_INVALID_ACCESS abort()
@@ -51,22 +54,38 @@ typedef enum {
 #define OP_INVALID_SIZE return MEMREG_RESULT_INVALID_SIZE
 #endif
 
-#define OP_IGNORE_LOAD_DATA if ((op) == OP_LOAD_DATA) { return MEMREG_RESULT_OK; }
+#define OP_IGNORE_LOAD_DATA      \
+    if ((op) == OP_LOAD_DATA)    \
+    {                            \
+        return MEMREG_RESULT_OK; \
+    }
 
-#define OP_ASSERT_SIZE(op, size)  if ((op) != OP_READ_##size && (op) != OP_WRITE_##size) { OP_INVALID_SIZE; }
-#define OP_ASSERT_READ(op)  if ((op) < 0) { OP_INVALID_ACCESS; }
-#define OP_ASSERT_WRITE(op) if ((op) > 0) { OP_INVALID_ACCESS; }
+#define OP_ASSERT_SIZE(op, size)                           \
+    if ((op) != OP_READ_##size && (op) != OP_WRITE_##size) \
+    {                                                      \
+        OP_INVALID_SIZE;                                   \
+    }
+#define OP_ASSERT_READ(op) \
+    if ((op) < 0)          \
+    {                      \
+        OP_INVALID_ACCESS; \
+    }
+#define OP_ASSERT_WRITE(op) \
+    if ((op) > 0)           \
+    {                       \
+        OP_INVALID_ACCESS;  \
+    }
 
-#define OP_RETURN_REG_RESULT(reg, size, result)     \
-    do                                              \
-    {                                               \
-        if ((op) == OP_READ_##size)                 \
-            *value = (reg);                         \
-        else if ((op) == OP_WRITE_##size)           \
-            (reg) = *value;                         \
-        else                                        \
-            return MEMREG_RESULT_INVALID_SIZE;      \
-        return (result);                    \
+#define OP_RETURN_REG_RESULT(reg, size, result) \
+    do                                          \
+    {                                           \
+        if ((op) == OP_READ_##size)             \
+            *value = (reg);                     \
+        else if ((op) == OP_WRITE_##size)       \
+            (reg) = *value;                     \
+        else                                    \
+            return MEMREG_RESULT_INVALID_SIZE;  \
+        return (result);                        \
     } while (0)
 
 #define OP_RETURN_REG(reg, size) OP_RETURN_REG_RESULT(reg, size, MEMREG_RESULT_OK)
@@ -122,5 +141,5 @@ inline static uint8_t memory_map_read_byte(memory_map_t *map, uint32_t addr)
 
 inline static void memory_map_write(memory_map_t *map, uint32_t addr, uint32_t value, byte_size_t size)
 {
-    memory_map_do_operation(map, addr, -size, &value);
+    memory_map_do_operation(map, addr, (memreg_op_t)-size, &value);
 }
